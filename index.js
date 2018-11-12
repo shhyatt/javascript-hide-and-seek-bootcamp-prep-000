3876afb03e070d6455255275cd068600155e79a6
Samantha Hyatt <shhyatt@gmail.com>
2018-11-12 17:33:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var list = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i<list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML, 10) + n;
+  }
+  
+}
+
+function deepestChild() {
+  
+}
 
